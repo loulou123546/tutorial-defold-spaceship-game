a3c7b5d9cb84539eda619ac001d6e6b36e9b66f9
Louis Maillard <louis.maillard@protonmail.com>
2024-07-18 20:58:17 -0400

    Initial relase: v0.1.0
    Features include:
    - Tutorial code from "Side scroller tutorial"
    - Control spaceship with up and down
    - collect stars and super stars
    - dash with space bar

---
+++ stars/factory.go
@@ -0,0 +1,53 @@
+components {
+  id: "script"
+  component: "/stars/factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "star_factory"
+  type: "factory"
+  data: "prototype: \"/stars/star.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bonus_star_factory"
+  type: "factory"
+  data: "prototype: \"/stars/bonus_star.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
